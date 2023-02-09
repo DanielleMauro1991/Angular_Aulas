00aa1f2e5bd9ffba8706c83f3a0641a0410758a1
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2023-02-09 14:15:58 -0300

    Modulo2
    Criação de módulo dentro de módulo

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiroComponent,
-    MeuPrimeiro2Component
+    MeuPrimeiro2Component,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,

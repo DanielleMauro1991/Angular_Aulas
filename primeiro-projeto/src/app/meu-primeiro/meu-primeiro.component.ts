2f0c3124d5593a68271a689f8c1573f84b3c2e11
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2023-02-09 12:07:28 -0300

    Component
    Criando o primeiro componen

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'meu-primeiro-component',
+    template: `
+    <P>Meu primeiro component com Angular!</P>
+    `,
+})
+
+export class MeuPrimeiroComponent {}
